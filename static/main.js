3f88f955483cf8cf765e258aa204333854699dcd
Yuxuama <matthieulecomte@free.fr>
2021-12-31 13:57:06 +0100

    basic behavior finished

--- static/main.js
+++ static/main.js
@@ -3,21 +3,21 @@ var context = true;
 var lessonContext = false;
 var n = 0;
 
-document.addEventListener('beforeinput', update)
+document.addEventListener('beforeinput', update);
 
 function submit() {
     const number = document.getElementById("number").value;
     const theme = document.getElementById("theme").value;
     const explain = document.getElementById('status');
 
-    if(n == '') {
+    if(n === '') {
         alert("Le paramètre du nombre d'élément n'est pas valide")
         return
     }
 
     if (!lessonContext) {
-        explain.innerHTML = "Tu as choisit le thème: " + theme
-        lessonContext = true
+        explain.innerHTML = "Tu as choisit le thème: " + theme;
+        lessonContext = true;
         lesson(number , theme);
     }
 }
@@ -29,25 +29,32 @@ async function lesson(number , theme) {
         DataType: 'json',
         headers: {'Access-Control-Allow-Origin': '*'},
     });
-    datas = await promise
+    datas = await promise;
     update(
         {target: {id: NaN}},
         true
-    )
+    );
 }
 
 function update(event, initial=false) {
+    // Update the container of the lesson whenever the enter key is pressed in the input context
     if (event.target.id === 'answer' && event.inputType === "insertLineBreak" && lessonContext && !initial){
         const target = document.getElementById('word');
+        const counter = document.getElementById('counter');
+        document.getElementById('answer').value = '';
+        
         if (n >= datas.length) {
+            document.getElementById('status').innerHTML = '';
+            counter.innerHTML = ''
             lessonContext = false;
             n = 0;
-            target.innerHTML = "Tu as finis la scéance d'entrainement"
-            return
+            target.innerHTML = "Tu as finis la scéance d'entrainement";
+            return;
         }
 
         if (context) {
             target.innerHTML = "Traduis: " + datas[n].word;
+            counter.innerHTML = (n+1).toString() + '/' + datas.length;
             context = false;
         } else {
             target.innerHTML = "La traduction est: " + datas[n].trad;
@@ -58,6 +65,8 @@ function update(event, initial=false) {
 
     else if (initial) {
         const target = document.getElementById('word');
+        const counter = document.getElementById('counter');
+        counter.innerHTML = (n+1).toString() + '/' + datas.length;
         target.innerHTML = "Traduis: " + datas[n].word;
         context = false;
     }
